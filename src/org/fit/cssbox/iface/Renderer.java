dd28e444740f59d91e80725d1887112ab261aa6b
Radek Burget <radkovo@users.sf.net>
2009-02-05 14:58:14 +0000

    SVG output of text boxes

--- src/org/fit/cssbox/iface/Renderer.java
+++ src/org/fit/cssbox/iface/Renderer.java
@@ -20,9 +20,11 @@
  */
 package org.fit.cssbox.iface;
 
+import java.awt.Color;
+import java.awt.Rectangle;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.Writer;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -30,8 +32,7 @@ import javax.imageio.ImageIO;
 
 import org.fit.cssbox.css.CSSNorm;
 import org.fit.cssbox.css.DOMAnalyzer;
-import org.fit.cssbox.layout.BrowserCanvas;
-import org.fit.cssbox.layout.ReplacedImage;
+import org.fit.cssbox.layout.*;
 import org.w3c.dom.Document;
 import org.w3c.tidy.Tidy;
 
@@ -47,14 +48,14 @@ public class Renderer
     public static final short TYPE_SVG = 1;
     
     /**
-     * Renders the URL and prints the result to the specified writer in the specified
+     * Renders the URL and prints the result to the specified output stream in the specified
      * format.
      * @param urlstring the source URL
-     * @param out output writer
+     * @param out output stream
      * @param type output type, one of the TYPE_XXX constants
      * @return true in case of success, false otherwise
      */
-    public boolean renderURL(String urlstring, Writer out, short type)
+    public boolean renderURL(String urlstring, OutputStream out, short type)
     {
         try {
             if (!urlstring.startsWith("http:") &&
@@ -89,6 +90,14 @@ public class Renderer
                 BrowserCanvas contentCanvas = new BrowserCanvas(da.getBody(), da, new java.awt.Dimension(1000, 600), url);
                 ImageIO.write(contentCanvas.getImage(), "png", out);
             }
+            else if (type == TYPE_SVG)
+            {
+                ReplacedImage.setLoadImages(true);
+                BrowserCanvas contentCanvas = new BrowserCanvas(da.getBody(), da, new java.awt.Dimension(1000, 600), url);
+                PrintWriter w = new PrintWriter(out);
+                writeSVG(contentCanvas.getViewport(), w);
+                w.close();
+            }
             
             return true;
             
@@ -98,5 +107,57 @@ public class Renderer
             return false;
         }
     }
+    
+    private void writeSVG(Viewport vp, PrintWriter out)
+    {
+        int w = vp.getContentWidth();
+        int h = vp.getContentHeight();
+        out.println("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"no\"?>");
+        out.println("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 20010904//EN\" \"http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd\">");
+        out.println("<svg xmlns=\"http://www.w3.org/2000/svg\"");
+        out.println("     xmlns:xlink=\"http://www.w3.org/1999/xlink\" xml:space=\"preserve\"");
+        out.println("         width=\"" + w + "\" height=\"" + h + "px\"");
+        out.println("         viewBox=\"0 0 " + w + " " + h + "\"");
+        out.println("         zoomAndPan=\"disable\" >");
+        
+        for (int i = 0; i < vp.getSubBoxNumber(); i++)
+            writeBoxSVG(vp.getSubBox(i), out);
+        
+        out.println("</svg>");    
+    }
+    
+    private void writeBoxSVG(Box box, PrintWriter out)
+    {
+        Rectangle b = box.getAbsoluteBounds();
+        if (box instanceof TextBox)
+        {
+            TextBox t = (TextBox) box;
+            VisualContext ctx = t.getVisualContext();
+            String style = "font-size:" + ctx.getFont().getSize() + "px;" + 
+                           "font-weight:" + (ctx.getFont().isBold()?"bold":"normal") + ";" + 
+                           "font-variant:" + (ctx.getFont().isItalic()?"italic":"normal") + ";" +
+                           "font-family:" + ctx.getFont().getFamily() + ";" +
+                           "fill:" + colorString(ctx.getColor()) + ";" +
+                           "stroke:none";
+                           
+            out.println("<text x=\"" + b.x + "\" y=\"" + b.y + "\" width=\"" + b.width + "\" height=\"" + b.height + "\" style=\"" + style + "\">" + htmlEntities(t.getText()) + "</text>"); 
+        }
+        else
+        {
+            ElementBox eb = (ElementBox) box;
+            for (int i = 0; i < eb.getSubBoxNumber(); i++)
+                writeBoxSVG(eb.getSubBox(i), out);
+        }
+    }
 
+    
+    private String colorString(Color color)
+    {
+        return String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
+    }
+    
+    private String htmlEntities(String s)
+    {
+        return s.replaceAll(">", "&gt;").replaceAll("<", "&lt;").replaceAll("&", "&amp;");
+    }
 }
