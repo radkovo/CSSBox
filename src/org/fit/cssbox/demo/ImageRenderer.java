a0b89d382c012c10d9b07656e041fd4c02551df8
Radek Burget <radkovo@users.sf.net>
2009-05-22 15:28:13 +0000

    ImageRenderer demo - slight interface change

--- src/org/fit/cssbox/demo/ImageRenderer.java
+++ src/org/fit/cssbox/demo/ImageRenderer.java
@@ -71,57 +71,50 @@ public class ImageRenderer
      * @param type output type, one of the TYPE_XXX constants
      * @return true in case of success, false otherwise
      */
-    public boolean renderURL(String urlstring, OutputStream out, short type)
+    public boolean renderURL(String urlstring, OutputStream out, short type) throws IOException
     {
-        try {
-            if (!urlstring.startsWith("http:") &&
-                !urlstring.startsWith("ftp:") &&
-                !urlstring.startsWith("file:"))
-                    urlstring = "http://" + urlstring;
-            
-            URL url = new URL(urlstring);
-            URLConnection con = url.openConnection();
-            con.setRequestProperty("User-Agent", "Mozilla/5.0 (compatible; Transformer/2.x; Linux) CSSBox/2.x (like Gecko)");
-            InputStream is = con.getInputStream();
-            
-            Tidy tidy = new Tidy();
-            tidy.setTrimEmptyElements(false);
-            tidy.setAsciiChars(false);
-            tidy.setInputEncoding("iso-8859-2");
-            //tidy.setInputEncoding("utf-8");
-            tidy.setXHTML(true);
-            Document doc = tidy.parseDOM(is, null);
-            
-            DOMAnalyzer da = new DOMAnalyzer(doc, url);
-            da.attributesToStyles();
-            da.addStyleSheet(null, CSSNorm.stdStyleSheet());
-            da.addStyleSheet(null, CSSNorm.userStyleSheet());
-            da.getStyleSheets();
-            
-            is.close();
-    
-            if (type == TYPE_PNG)
-            {
-                ReplacedImage.setLoadImages(true);
-                BrowserCanvas contentCanvas = new BrowserCanvas(da.getRoot(), da, new java.awt.Dimension(1000, 600), url);
-                ImageIO.write(contentCanvas.getImage(), "png", out);
-            }
-            else if (type == TYPE_SVG)
-            {
-                ReplacedImage.setLoadImages(true);
-                BrowserCanvas contentCanvas = new BrowserCanvas(da.getRoot(), da, new java.awt.Dimension(1000, 600), url);
-                PrintWriter w = new PrintWriter(new OutputStreamWriter(out, "utf-8"));
-                writeSVG(contentCanvas.getViewport(), w);
-                w.close();
-            }
-            
-            return true;
-            
-        } catch (Exception e) {
-            System.err.println("*** Error: "+e.getMessage());
-            e.printStackTrace();
-            return false;
+        if (!urlstring.startsWith("http:") &&
+            !urlstring.startsWith("ftp:") &&
+            !urlstring.startsWith("file:"))
+                urlstring = "http://" + urlstring;
+        
+        URL url = new URL(urlstring);
+        URLConnection con = url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0 (compatible; Transformer/2.x; Linux) CSSBox/2.x (like Gecko)");
+        InputStream is = con.getInputStream();
+        
+        Tidy tidy = new Tidy();
+        tidy.setTrimEmptyElements(false);
+        tidy.setAsciiChars(false);
+        tidy.setInputEncoding("iso-8859-2");
+        //tidy.setInputEncoding("utf-8");
+        tidy.setXHTML(true);
+        Document doc = tidy.parseDOM(is, null);
+        
+        DOMAnalyzer da = new DOMAnalyzer(doc, url);
+        da.attributesToStyles();
+        da.addStyleSheet(null, CSSNorm.stdStyleSheet());
+        da.addStyleSheet(null, CSSNorm.userStyleSheet());
+        da.getStyleSheets();
+        
+        is.close();
+
+        if (type == TYPE_PNG)
+        {
+            ReplacedImage.setLoadImages(true);
+            BrowserCanvas contentCanvas = new BrowserCanvas(da.getRoot(), da, new java.awt.Dimension(1000, 600), url);
+            ImageIO.write(contentCanvas.getImage(), "png", out);
         }
+        else if (type == TYPE_SVG)
+        {
+            ReplacedImage.setLoadImages(true);
+            BrowserCanvas contentCanvas = new BrowserCanvas(da.getRoot(), da, new java.awt.Dimension(1000, 600), url);
+            PrintWriter w = new PrintWriter(new OutputStreamWriter(out, "utf-8"));
+            writeSVG(contentCanvas.getViewport(), w);
+            w.close();
+        }
+        
+        return true;
     }
     
     private void writeSVG(Viewport vp, PrintWriter out) throws IOException
