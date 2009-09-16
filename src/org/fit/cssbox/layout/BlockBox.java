abe9d65a34c765f26d1a5818b1879896a9166769
Radek Burget <radkovo@users.sf.net>
2009-09-16 16:43:23 +0000

    Box clipping for the viewport size fixed

--- src/org/fit/cssbox/layout/BlockBox.java
+++ src/org/fit/cssbox/layout/BlockBox.java
@@ -966,7 +966,10 @@ public class BlockBox extends ElementBox
             
             if (isDisplayed())
             {
-                viewport.updateBoundsFor(getClippedBounds());
+                if (clipblock == viewport)
+                    viewport.updateBoundsFor(absbounds);
+                else
+                    viewport.updateBoundsFor(getClippedBounds());
                 
                 //repeat for all valid subboxes
                 for (int i = startChild; i < endChild; i++)
