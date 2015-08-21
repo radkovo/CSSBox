2a0e345b70cd2823882c358573cd8fb5bc9e581d
Radek Burget <burgetr@fit.vutbr.cz>
2015-08-21 23:17:34 +0200

    Fixed replaced element width/height computation (consider max/min widths/heights)

--- src/main/java/org/fit/cssbox/layout/CSSDecoder.java
+++ src/main/java/org/fit/cssbox/layout/CSSDecoder.java
@@ -154,20 +154,14 @@ public class CSSDecoder
         int atrw = -1;
         int atrh = -1;
         try {
-            if (!HTMLNorm.getAttribute(el, "width").equals("")) {
+            if (!HTMLNorm.getAttribute(el, "width").equals(""))
                 atrw = HTMLNorm.computeAttributeLength(HTMLNorm.getAttribute(el, "width"), twidth);
-            } else {
-                atrw = twidth;
-            }
         } catch (NumberFormatException e) {
             log.info("Invalid width value: " + HTMLNorm.getAttribute(el, "width"));
         }
         try {
-            if (!HTMLNorm.getAttribute(el, "height").equals("")) {
+            if (!HTMLNorm.getAttribute(el, "height").equals(""))
                 atrh = HTMLNorm.computeAttributeLength(HTMLNorm.getAttribute(el, "height"), theight);
-            } else {
-                atrh = theight;
-            }
         } catch (NumberFormatException e) {
             log.info("Invalid height value: " + HTMLNorm.getAttribute(el, "width"));
         }
@@ -200,11 +194,19 @@ public class CSSDecoder
         if (width == CSSProperty.Width.AUTO) width = null; //auto and null are equal for width
         CSSProperty.Height height = box.getStyle().getProperty("height");
         if (height == CSSProperty.Height.AUTO) height = null; //auto and null are equal for height
-        if (width == null && height != null)
+        if (width == null && height == null)
+        {
+            final int[] result = applyCombinedLimits(boxw, boxh, box, dec, twidth, theight);
+            boxw = result[0];
+            boxh = result[1];
+        }
+        else if (width == null && height != null)
         {
             //compute boxh, boxw is intrinsic
             int autoh = Math.round(boxw / intr);
             boxh = dec.getLength(box.getLengthValue("height"), height == CSSProperty.Height.AUTO, boxh, autoh, theight);
+            boxh = applyHeightLimits(boxh, box, dec, theight);
+            //boxw intrinsic value
             boxw = Math.round(intr * boxh);
         }
         else if (width != null && height == null)
@@ -212,16 +214,154 @@ public class CSSDecoder
             //compute boxw, boxh is intrinsic
             int autow = Math.round(intr * boxh);
             boxw = dec.getLength(box.getLengthValue("width"), width == CSSProperty.Width.AUTO, boxw, autow, twidth);
+            boxw = applyWidthLimits(boxw, box, dec, twidth);
+            //boxh intrinsic value
             boxh = Math.round(boxw / intr);
         }
         else
         {
             boxw = dec.getLength(box.getLengthValue("width"), width == CSSProperty.Width.AUTO, boxw, intw, twidth);
+            boxw = applyWidthLimits(boxw, box, dec, twidth);
             boxh = dec.getLength(box.getLengthValue("height"), height == CSSProperty.Height.AUTO, boxh, inth, theight);
+            boxh = applyHeightLimits(boxh, box, dec, theight);
         }
         
         return new Rectangle(boxw, boxh);
     }
 
+    public static int applyWidthLimits(int width, ElementBox box, CSSDecoder dec, int twidth)
+    {
+        int ret = width;
+        CSSProperty.MaxWidth max = box.getStyle().getProperty("max-width");
+        if (max != null && max != CSSProperty.MaxWidth.NONE)
+        {
+            final int maxval = dec.getLength(box.getLengthValue("max-width"), false, -1, -1, twidth);
+            if (ret > maxval)
+                ret = maxval;
+        }
+        CSSProperty.MinWidth min = box.getStyle().getProperty("min-width");
+        if (min != null)
+        {
+            final int minval = dec.getLength(box.getLengthValue("min-width"), false, -1, -1, twidth);
+            if (ret < minval)
+                ret = minval;
+        }
+        return ret;
+    }
+    
+    public static int applyHeightLimits(int height, ElementBox box, CSSDecoder dec, int theight)
+    {
+        int ret = height;
+        CSSProperty.MaxHeight max = box.getStyle().getProperty("max-height");
+        if (max != null && max != CSSProperty.MaxHeight.NONE)
+        {
+            final int maxval = dec.getLength(box.getLengthValue("max-height"), false, -1, -1, theight);
+            if (ret > maxval)
+                ret = maxval;
+        }
+        CSSProperty.MinHeight min = box.getStyle().getProperty("min-height");
+        if (min != null)
+        {
+            final int minval = dec.getLength(box.getLengthValue("min-height"), false, -1, -1, theight);
+            if (ret < minval)
+                ret = minval;
+        }
+        return ret;
+    }
+    
+    public static int[] applyCombinedLimits(int w, int h, ElementBox box, CSSDecoder dec, int twidth, int theight)
+    {
+        //decode min/max values from the style
+        final CSSProperty.MinWidth pminw = box.getStyle().getProperty("min-width");
+        final CSSProperty.MaxWidth pmaxw = box.getStyle().getProperty("max-width");
+        final CSSProperty.MinHeight pminh = box.getStyle().getProperty("min-height");
+        final CSSProperty.MaxHeight pmaxh = box.getStyle().getProperty("max-height");
+        final int minw, maxw, minh, maxh;
+        if (pminw != null)
+            minw = dec.getLength(box.getLengthValue("min-width"), false, 0, 0, twidth);
+        else
+            minw = 0;
+        if (pmaxw != null && pmaxw != CSSProperty.MaxWidth.NONE)
+            maxw = Math.max(minw, dec.getLength(box.getLengthValue("max-width"), false, 0, 0, twidth)); //maxw >= minw musth hold
+        else
+            maxw = Integer.MAX_VALUE;
+        if (pminh != null)
+            minh = dec.getLength(box.getLengthValue("min-height"), false, 0, 0, theight);
+        else
+            minh = 0;
+        if (pmaxh != null && pmaxh != CSSProperty.MaxHeight.NONE)
+            maxh = Math.max(minh, dec.getLength(box.getLengthValue("max-height"), false, 0, 0, theight));
+        else
+            maxh = Integer.MAX_VALUE;
+        
+        //apply the CSS2 constraints from the table
+        //http://www.w3.org/TR/CSS21/visudet.html#min-max-widths
+        final float hwr = (float) h / w;
+        final float whr = (float) w / h;
+        final int retw, reth;
+        if (w > maxw && h < minh) 
+        { 
+            retw = maxw;
+            reth = minh; 
+        }
+        else if (w < minw && h > maxh)
+        { 
+            retw = minw;
+            reth = maxh; 
+        }
+        else if (w < minw && h < minh)
+        {
+            if ((float) minw / w > (float) minh / h)
+            {
+                retw = minw;
+                reth = Math.min(maxh, Math.round(minw * hwr));
+            }
+            else
+            {
+                retw = Math.min(maxw, Math.round(minh * whr));
+                reth = minh;
+            }
+        }
+        else if (w > maxw && h > maxh)
+        {
+            if ((float) maxw / w > (float) maxh / h)
+            {
+                retw = Math.max(minw, Math.round(maxh * whr));
+                reth = maxh;
+            }
+            else
+            {
+                retw = maxw;
+                reth = Math.max(minh, Math.round(maxw * hwr));
+            }
+        }
+        else if (h < minh)
+        {
+            retw = Math.min(Math.round(minh * whr), maxw);
+            reth = minh;
+        }
+        else if (h > maxh)
+        {
+            retw = Math.max(Math.round(maxh * whr), minw);
+            reth = maxh;
+        }
+        else if (w < minw)
+        {
+            retw = minw;
+            reth = Math.min(Math.round(minw * hwr), maxh);
+        }
+        else if (w > maxw)
+        {
+            retw = maxw;
+            reth = Math.max(Math.round(maxw * hwr), minh);
+        }
+        else
+        {
+            retw = w;
+            reth = h;
+        }
+        
+        return new int[]{retw, reth};
+    }
     
 }
