0753b9a8b832e8f900f21a63c65192cb4fa3d4d5
Radek Burget <burgetr@fit.vutbr.cz>
2016-01-26 09:50:57 +0100

    Dependency installation for testing

--- misc/install_dependencies.sh
+++ misc/install_dependencies.sh
@@ -11,6 +11,4 @@ mkdir $PARSER
 
 git clone https://github.com/radkovo/CSSBoxTesting.git $TESTING
 git clone https://github.com/radkovo/jStyleParser.git $PARSER
-(cd $PARSER; mvn -DskipTests=true -Dmaven.javadoc.skip=true package deploy)
-
-mvn test
+(cd $PARSER; mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V)
