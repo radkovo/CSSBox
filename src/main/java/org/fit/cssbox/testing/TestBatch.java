4a324ecaf2580e97fd4c00289f6c1713c363b34b
Radek Burget <burgetr@fit.vutbr.cz>
2016-03-10 13:26:00 +0100

    Test memory usage tuning

--- src/main/java/org/fit/cssbox/testing/TestBatch.java
+++ src/main/java/org/fit/cssbox/testing/TestBatch.java
@@ -310,6 +310,7 @@ public class TestBatch
             long free = runtime.freeMemory();
             if (free < minFree) minFree = free;
             list.set(i, null); //do not hold the tests in memory
+            System.gc();
             System.out.println(" free:" + (free/1000) + " min:" + (minFree/1000));
         }
     }
