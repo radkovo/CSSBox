fd0f976a0377a2f8ab662b4b7bb420d82984ceef
Sergey Ushakov <s-n-ushakov@yandex.ru>
2017-01-21 01:12:21 +0300

    creation of empty 'style' attributes suppressed

--- src/main/java/org/fit/cssbox/css/DOMAnalyzer.java
+++ src/main/java/org/fit/cssbox/css/DOMAnalyzer.java
@@ -623,7 +623,8 @@ public class DOMAnalyzer
             if (decl != null)
             {
                 String decls = decl.toString().replace("\n", "");
-                el.setAttribute("style", quote(decls));
+                if (!decls.isEmpty()) //+usn
+                    el.setAttribute("style", quote(decls)); //~usn
             }
         }                
         NodeList child = n.getChildNodes();
